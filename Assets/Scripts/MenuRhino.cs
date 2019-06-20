6b720989d4897f0fa37483e9c3a0d20505cdc382
BrianPickens <brianpickens53@gmail.com>
2019-06-19 22:05:59 -0700

    updating rhino animations

---
+++ Assets/Scripts/MenuRhino.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuRhino : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator myAnimator = null;
+
+    [SerializeField]
+    private float timeBetweenAnimations = 7f;
+
+    private float currentTime = 0f;
+
+    private void Start()
+    {
+        currentTime = timeBetweenAnimations;   
+    }
+
+    private void Update()
+    {
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0)
+        {
+            PlayRandomAnimation();
+            currentTime = timeBetweenAnimations;
+        }
+    }
+
+    private void PlayRandomAnimation()
+    {
+        int animation = Random.Range(0, 3);
+
+        switch (animation)
+        {
+            case 0:
+                myAnimator.SetTrigger("ThumbsUp");
+                break;
+
+            case 1:
+                myAnimator.SetTrigger("Shake");
+                break;
+
+            case 2:
+                myAnimator.SetTrigger("Flex");
+                break;
+
+            default:
+                myAnimator.SetTrigger("Shake");
+                break;
+        }
+    }
+
+}
